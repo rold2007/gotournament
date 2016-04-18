9c6e1ba8fde5caa12f78959f46e38893e246c12b
oneekz <oneekz@live.com>
2016-04-19 01:01:31 +0200

    Registered missing file in the project file

--- GoTournament/Adjudicator.cs
+++ GoTournament/Adjudicator.cs
@@ -221,14 +221,4 @@ namespace GoTournament
     }
 
     public enum EndGameReason { None, MoveTimeOut, Resign, InvalidMove, ConsecutivePass }
-
-    public class GameStatistic
-    {
-        public EndGameReason EndReason { get; set; }
-        public string WinnerName { get; set; }
-        public int TotalMoves { get; set; }
-        public string FinalBoard { get; set; }
-        public bool WhiteFinishedGame { get; set; }
-        public string GameFinisherName { get; set; }
-    }
 }
